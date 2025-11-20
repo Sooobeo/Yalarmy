33f678ca2da9646e8e9dc231c00496888fcfdf2d
Sooobeo <tengerined4@naver.com>
2025-11-20 13:24:46 +0900

    login&adding courses

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'pages/auth_page.dart';
 import 'pages/home_page.dart';
 
 // ⚠️ 여기에 네 Supabase 프로젝트 URL / anon key 넣기
-const supabaseUrl = 'https://네프로젝트주소.supabase.co';
-const supabaseAnonKey = '여기에_anon_public_key';
+const supabaseUrl = 'https://sguedpyifsjqzjhdaqzb.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNndWVkcHlpZnNqcXpqaGRhcXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwMzE1NDYsImV4cCI6MjA3NTYwNzU0Nn0.iggfDZwVS9E2MhTIl-9gRDVLZ4ermKCoW43mL-fAl7Q';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
