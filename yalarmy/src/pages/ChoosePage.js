2aa3e2bfed97b2df8e02f315415b80c42313b770
JW <parkjinwoo037@gmail.com>
2025-11-22 04:48:10 +0900

    ..

--- yalarmy/src/pages/ChoosePage.js
+++ yalarmy/src/pages/ChoosePage.js
@@ -24,23 +24,23 @@ function ChoosePage()
 
             <div className = "flex flex-col pt-[20em] pb-[9em] px-[3em] gap-[20rem] bg-[#fcf7fe] items-center">
                 <div className = "flex flex-col gap-6">
-                    <div data-aos="fade-up" data-aos-distance="20" className = "opacity-0 text-2xl font-semibold">
+                    <div data-aos="fade-up" className = "opacity-0 text-2xl font-semibold">
                         👋 안녕하세요, 오늘도 학업 일정 시작해볼까요?
                     </div>
                     <div className = "flex flex-col gap-2">
-                        <p data-aos="fade-up" data-aos-distance="100" className = "opacity-0">📌 이번 주 마감 일정: 4개</p>
-                        <p data-aos="fade-up" data-aos-distance="100" className = "opacity-0 text-red-400">⏳ 오늘 마감되는 항목: 1개</p>
+                        <p data-aos="fade-up" className = "opacity-0">📌 이번 주 마감 일정: 4개</p>
+                        <p data-aos="fade-up" className = "opacity-0 text-red-400">⏳ 오늘 마감되는 항목: 1개</p>
                     </div>
                 </div>
 
 
                 <div className = "flex flex-row justify-evenly w-full">
-                    <Link to = "/calendar" data-aos="fade-up" data-aos-distance="100" className = "opacity-0 flex flex-col gap-2">
+                    <Link to = "/calendar" data-aos="fade-up" className = "opacity-0 flex flex-col gap-2">
                         <h2 className = "text-xl font-semibold">[📅 캘린더]</h2>
                         일정을 한 번에 보고 관리하세요.
                     </Link>
 
-                    <Link to = "/tasks" data-aos="fade-up" data-aos-distance="100" className = "opacity-0 flex flex-col gap-2">
+                    <Link to = "/tasks" data-aos="fade-up" className = "opacity-0 flex flex-col gap-2">
                         <h2 className = "text-xl font-semibold">[📝 미완료 항목]</h2>
                         마감 기한이 임박한 과제/영상만 모아봤어요.
                     </Link>
