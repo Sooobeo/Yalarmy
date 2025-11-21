9b9db71df14338693c6189085ff328a7c4e4b29a
JW <parkjinwoo037@gmail.com>
2025-11-22 06:06:01 +0900

    .

--- yalarmy/src/App.js
+++ yalarmy/src/App.js
@@ -3,23 +3,23 @@ import Calendar from './pages/Calendarr.js';
 import ChoosePage from './pages/ChoosePage.js';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import LandingPage from "./components/LandingPage";
-import LoginPage from "./components/LoginPage";
-import SignupPage from "./components/SignupPage";
+//import LandingPage from "./components/LandingPage";
+//import LoginPage from "./components/LoginPage";
+//import SignupPage from "./components/SignupPage";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          {/*<Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
-          {/*
+          */}
           <Route path = "/" element = {<ChoosePage/>}></Route>
           <Route path = "/calendar" element = {<Calendar/>}></Route>
           <Route path = "/tasks" element = {<Unfinished/>}></Route>
-          */}
+          
         </Routes>
       </div>
     </Router>
